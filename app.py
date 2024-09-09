56e9ab5ee1c34983c24e86edd065cc0a874a1542
Swethamercy <62125939+Swethamercy@users.noreply.github.com>
2024-09-09 10:58:58 +0000

    added HTML and CSS

--- app.py
+++ app.py
@@ -9,13 +9,13 @@ def hello():
 if __name__ == ' __main__':
   app.run(host='0.0.0.0', debug=True)
   '''
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, Swetha"
+    return render_template('home.html')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
